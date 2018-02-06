4802d06f9507b118f99508cbf44ece8110272326
Jacob <jacob.lee.yent@gmail.com>
2018-02-06 14:35:51 -0800

    Queue 04-06
    Finished queue problems 04 -06, starting binary problems

---
+++ tree_binary/template_tree.cpp
@@ -0,0 +1,22 @@
+//PROBLEM:
+//
+//EARLY NOTES:
+//
+//PLANNING:
+//
+//FINAL NOTES:
+
+#include <iostream>
+
+using namespace std;
+
+//our basic node structure
+struct Node {
+	int key;
+	Node *left, *right;
+};
+
+int main()
+{
+	return 0;
+}
