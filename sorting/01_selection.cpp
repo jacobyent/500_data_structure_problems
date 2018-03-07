ff01630a9ca0f8f234ab7a081871b99138fb6126
Jacob <jacob.lee.yent@gmail.com>
2018-03-07 15:10:27 -0800

    Sorting 00-01

---
+++ sorting/01_selection.cpp
@@ -0,0 +1,18 @@
+//PROBLEM: Implement selction sort
+//
+//PLANNING: Two for loops, pass through the remaining unsorted array, find the smallest element and then swap that element into place.
+//
+//FINAL NOTES: 
+
+using namespace std;
+
+#include <iostream>
+
+void selection(int arr[], int n)
+{
+
+}
+int main()
+{
+	return 0;
+}
---
+++ sorting/template.cpp
@@ -0,0 +1,18 @@
+//PROBLEM:
+//
+//EARLY NOTES:
+//
+//PLANNING:
+//
+//FINAL NOTES: 
+
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	int arr[] = {3,5,8,4,1,9,-2};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	return 0;
+}
