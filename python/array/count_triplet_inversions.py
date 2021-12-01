30436dacabbe7d21e9956085e818de10883d5aa5
Jacob Yent <jacobyent@Jacobs-MacBook-Pro.local>
2021-11-30 21:13:34 -0800

    Nov 30th

--- python/array/count_triplet_inversions.py
+++ python/array/count_triplet_inversions.py
@@ -2,14 +2,22 @@
 
 # initial thoughts: linear time, constant space
 
+# I missunderstood the problem, we are suppoed to count every possible inversion. Which can be done in quadratic time and linear space
 
 import random 
 
 
 def chompy(arr):
     count = 0 
-    local_max = arr[0]
-    
+    local_max = -1000
+    mid = -999
+    for i in range(len(arr)):
+        if local_max < arr[i]:
+            local_max = arr[i]
+        elif mid < arr[i]:
+            mid = arr[i]
+        else: 
+            count += 1
     print(count) 
      
 
@@ -18,5 +26,7 @@ def main():
     array = []
     for i in range(N):
         array.append(random.randint(1,10))
-        
+    print(array) 
     chompy(array)
+    
+main()
